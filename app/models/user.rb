c19b57036aa3bbdb07a01a6efe8c1a58f291814f
sergocap <systemofadown.2013@yandex.ru>
2016-11-06 14:44:00 +0700

    style for form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   has_attached_file :avatar, default_url: '/images/missing.png', :styles => { :small => "64x64^" }, :convert_options => { :small => "-gravity center -extent 64x64" }
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"], :message => 'Формат файла должен быть jpg/jpeg/png/gif. '
-  validates_attachment_size :avatar, in: 0..100.kilobytes, :message => 'Размер файла должен быть менее 100 КБ. '
+  validates_attachment_size :avatar, in: 0..500.kilobytes, :message => 'Размер файла должен быть менее 500 КБ. '
   has_many :places
   before_destroy :destroy_attachments
 
